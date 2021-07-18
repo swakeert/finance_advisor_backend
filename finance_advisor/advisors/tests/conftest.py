306019dad94d83804d92e5b6768e15df70ba09df
Swakeert Jain <swakeert@gmail.com>
2021-07-18 19:11:49 +0530

    Fix lint and improve pytest

--- finance_advisor/advisors/tests/conftest.py
+++ finance_advisor/advisors/tests/conftest.py
@@ -1,10 +1,6 @@
 import pytest
-from rest_framework.test import APIClient
 
-from finance_advisor.advisors.tests.factory import (
-    AdvisorFactory,
-    AdvisorRelationshipFactory,
-)
+from finance_advisor.advisors.tests.factory import AdvisorFactory
 
 
 @pytest.fixture
@@ -12,25 +8,3 @@ def mock_alternate_advisor_user():
     advisor = AdvisorFactory(email="advisor_1@email.com")
     advisor.set_password("test_password")
     return advisor
-
-
-@pytest.fixture
-def mock_advisor_user():
-    advisor = AdvisorFactory(email="advisor_0@email.com")
-    advisor.set_password("test_password")
-    return advisor
-
-
-@pytest.fixture
-def mock_relationship(mock_advisee_user, mock_advisor_user):
-    relationship = AdvisorRelationshipFactory(
-        advisee=mock_advisee_user, advisor=mock_advisor_user
-    )
-    return relationship
-
-
-@pytest.fixture
-def authenticated_api_client_as_advisor(mock_advisor_user):
-    api_client = APIClient()
-    api_client.force_authenticate(mock_advisor_user)
-    return api_client
--- finance_advisor/conftest.py
+++ finance_advisor/conftest.py
@@ -1,16 +1,70 @@
-from finance_advisor.advisors.tests.conftest import (
-    mock_advisor_user,
-    mock_relationship,
-    authenticated_api_client_as_advisor,
-)
+import pytest
+from rest_framework.test import APIClient
 
-from finance_advisor.advisees.tests.conftest import (
-    mock_advisee_user,
-    mock_alternate_advisee_user,
-    authenticated_api_client_as_advisee,
+from finance_advisor.advisees.tests.factory import AdviseeFactory
+from finance_advisor.advisors.tests.factory import (
+    AdvisorFactory,
+    AdvisorRelationshipFactory,
 )
+from finance_advisor.core.tests.factory import CurrencyFactory
 
-from finance_advisor.core.tests.conftest import (
-    mock_currency,
-    mock_alternate_currency,
-)
+
+@pytest.fixture
+def mock_advisee_user():
+    advisee = AdviseeFactory(email="advisee_0@email.com")
+    advisee.set_password("test_password")
+    return advisee
+
+
+@pytest.fixture
+def mock_alternate_advisee_user():
+    advisee = AdviseeFactory(email="advisee_1@email.com")
+    advisee.set_password("test_password")
+    return advisee
+
+
+@pytest.fixture
+def authenticated_api_client_as_advisee(mock_advisee_user):
+    api_client = APIClient()
+    api_client.force_authenticate(mock_advisee_user)
+    return api_client
+
+
+@pytest.fixture
+def mock_advisor_user():
+    advisor = AdvisorFactory(email="advisor_0@email.com")
+    advisor.set_password("test_password")
+    return advisor
+
+
+@pytest.fixture
+def mock_relationship(mock_advisee_user, mock_advisor_user):
+    relationship = AdvisorRelationshipFactory(
+        advisee=mock_advisee_user, advisor=mock_advisor_user
+    )
+    return relationship
+
+
+@pytest.fixture
+def authenticated_api_client_as_advisor(mock_advisor_user):
+    api_client = APIClient()
+    api_client.force_authenticate(mock_advisor_user)
+    return api_client
+
+
+@pytest.fixture
+def mock_currency():
+    return CurrencyFactory(
+        code="MCK",
+        numeric_code="123",
+        name="Mock Currency",
+    )
+
+
+@pytest.fixture
+def mock_alternate_currency():
+    return CurrencyFactory(
+        code="MOC",
+        numeric_code="567",
+        name="Mock Alternate Currency",
+    )
